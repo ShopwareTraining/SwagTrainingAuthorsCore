069ec159fc03e16d381efa4cde4b42dcf234663d
Rico Neitzel <rico@buro71a.de>
2021-08-01 15:32:14 +0200

    replaces executeUpdate with executeStatement

--- src/Migration/Migration1609743530.php
+++ src/Migration/Migration1609743530.php
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `author` (
     DEFAULT CHARSET = utf8mb4
     COLLATE = utf8mb4_unicode_ci;
 SQL;
-        $connection->executeUpdate($sql);
+        $connection->executeStatement($sql);
     }
 
     public function updateDestructive(Connection $connection): void
